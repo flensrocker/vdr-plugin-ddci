b611177cd59b14c050004a569a640724d4a2ff8f
Lars Hanisch <dvb@flensrocker.de>
2011-12-29 19:20:23 +0100

    refactoring of vdr patch

--- dddvbci.h
+++ dddvbci.h
@@ -27,7 +27,7 @@ private:
 public:
   cDdDvbCiAdapter(cDevice *Device, int FdCa, int FdSec, const char *AdapterNum, const char *DeviceNum, const char *DevNode);
   virtual ~cDdDvbCiAdapter(void);
-  virtual cTSBuffer *GetTSBuffer(int FdDvr);
+  virtual cTSBufferBase *GetTSBuffer(int FdDvr);
   virtual bool SetIdle(bool Idle, bool TestOnly);
   virtual bool IsIdle(void) const { return idle; }
 
